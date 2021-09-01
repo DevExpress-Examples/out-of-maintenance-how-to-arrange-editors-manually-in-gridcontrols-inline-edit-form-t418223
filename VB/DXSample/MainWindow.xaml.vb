8bb3a1c7f7040b4330e17c4c33e5ab7f56b6032b
DevExpressExampleBot <fakeEmail>
2021-09-01 13:59:31 +0300

    Source auto update [skip ci]

--- VB/DXSample/MainWindow.xaml.vb
+++ VB/DXSample/MainWindow.xaml.vb
@@ -1,15 +1,15 @@
 ﻿Imports DevExpress.Xpf.Core
 
 Namespace DXSample
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits DXWindow
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits DXWindow
 
-        Public Sub New()
-            InitializeComponent()
-            DataContext = DataHelper.GetRows()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+			DataContext = DataHelper.GetRows()
+		End Sub
+	End Class
 End Namespace
