732d3fe5c387fc53313c02993a3f076f93061ec8
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-12-21 15:34:32 +0400

    Change example status to out of maintenance

--- CS/DXSample/RowViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DXSample {
-    public class RowViewModel {
-        public virtual string City { get; set; }
-        public virtual double UnitPrice { get; set; }
-        public virtual double Quantity { get; set; }
-        public virtual double Discount { get; set; }
-        public virtual double Freight { get; set; }
-    }
-}
